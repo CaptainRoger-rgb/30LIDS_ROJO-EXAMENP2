a1871e771e264bafcbd6ce358a9c8d3f6b401ac4
CaptainRoger-rgb <144484861+CaptainRoger-rgb@users.noreply.github.com>
2023-11-09 00:31:59 -0600

    Agregar archivos de proyecto.

---
+++ 30LIDS_ROJO#EXAMENP2/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace _30LIDS_ROJO_EXAMENP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLimTem = new System.Windows.Forms.TextBox();
+            this.lblConetion = new System.Windows.Forms.Label();
+            this.lblTemperatura = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.ForeColor = System.Drawing.Color.Green;
+            this.btnConectar.Location = new System.Drawing.Point(12, 109);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(127, 47);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "CONECTAR";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncender.ForeColor = System.Drawing.Color.Green;
+            this.btnEncender.Location = new System.Drawing.Point(12, 323);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(127, 47);
+            this.btnEncender.TabIndex = 1;
+            this.btnEncender.Text = "ENCENDER";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.ForeColor = System.Drawing.Color.Red;
+            this.btnApagar.Location = new System.Drawing.Point(183, 323);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(127, 47);
+            this.btnApagar.TabIndex = 2;
+            this.btnApagar.Text = "APAGADO";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesconectar.ForeColor = System.Drawing.Color.Red;
+            this.btnDesconectar.Location = new System.Drawing.Point(318, 109);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(127, 47);
+            this.btnDesconectar.TabIndex = 3;
+            this.btnDesconectar.Text = "DECONECTAR";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Limite de Temperatura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(346, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Temperatura";
+            // 
+            // txtLimTem
+            // 
+            this.txtLimTem.Location = new System.Drawing.Point(12, 30);
+            this.txtLimTem.Name = "txtLimTem";
+            this.txtLimTem.Size = new System.Drawing.Size(100, 20);
+            this.txtLimTem.TabIndex = 6;
+            // 
+            // lblConetion
+            // 
+            this.lblConetion.AutoSize = true;
+            this.lblConetion.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConetion.ForeColor = System.Drawing.Color.Aqua;
+            this.lblConetion.Location = new System.Drawing.Point(8, 82);
+            this.lblConetion.Name = "lblConetion";
+            this.lblConetion.Size = new System.Drawing.Size(144, 24);
+            this.lblConetion.TabIndex = 7;
+            this.lblConetion.Text = "Sin Conexion";
+            // 
+            // lblTemperatura
+            // 
+            this.lblTemperatura.AutoSize = true;
+            this.lblTemperatura.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperatura.ForeColor = System.Drawing.Color.Orange;
+            this.lblTemperatura.Location = new System.Drawing.Point(334, 72);
+            this.lblTemperatura.Name = "lblTemperatura";
+            this.lblTemperatura.Size = new System.Drawing.Size(55, 34);
+            this.lblTemperatura.TabIndex = 8;
+            this.lblTemperatura.Text = "0.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(149, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Led ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(517, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "CONECCION DE BD";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(536, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(557, 62);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 12;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.Color.Green;
+            this.btnGuardar.Location = new System.Drawing.Point(539, 163);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(127, 47);
+            this.btnGuardar.TabIndex = 13;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(557, 124);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(536, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Apellido ";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.ForeColor = System.Drawing.Color.Red;
+            this.btnBorrar.Location = new System.Drawing.Point(539, 227);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(127, 47);
+            this.btnBorrar.TabIndex = 16;
+            this.btnBorrar.Text = "BORRAR";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTemperatura);
+            this.Controls.Add(this.lblConetion);
+            this.Controls.Add(this.txtLimTem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEncender);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EXAMEN 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLimTem;
+        private System.Windows.Forms.Label lblConetion;
+        private System.Windows.Forms.Label lblTemperatura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ 30LIDS_ROJO#EXAMENP2/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using MySql.Data.MySqlClient; //Referencia para trabajar en lOCALHOST en Xamp
+using System.Text.RegularExpressions;
+
+namespace _30LIDS_ROJO_EXAMENP2
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextDelegate(string value);
+        public SerialPort ArduinoPort
+        {
+            get;
+        }
+        //Datos de conexion a MySql (Xamp)
+        string conexionSql = "Server=localhost;Port=3306;Database=examenp2;Uid=root;Pwd=;";
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM8";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 500;
+            ArduinoPort.WriteTimeout = 500;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            this.btnConectar.Click += btnConectar_Click;
+            ArduinoPort.Open();
+            //Led Codigo 
+            //vincular Eventos
+            this.FormClosing += CerrandoForm1;
+            this.btnApagar.Click += btnApagar_Click;
+            this.btnEncender.Click += btnEncender_Click;
+
+            /*
+            txtNombre.TextChanged += ValidarNombre;
+            txtApellido.TextChanged += ValidarApellidos;*/
+        }
+        private void DataReceivedHandler(object sneder, SerialDataReceivedEventArgs e)
+        {
+            string dato = ArduinoPort.ReadLine();
+            EscribirTxt(dato);
+        }
+
+        private void EscribirTxt(string dato)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new SetTextDelegate(EscribirTxt), dato);
+                }
+                catch { }
+            else
+                lblTemperatura.Text = dato;
+        }
+
+        //Insertar Y validar Registros de nombre
+        private void InsertarRegistro(string nombre, string apellidos)
+        {
+            using (MySqlConnection conection = new MySqlConnection(conexionSql))
+            {
+                conection.Open();
+
+                string insertQuery = "INSERT INTO registros (Nombre, Apellidos)" + "VALUES (@Nombre, @Apellidos)";                
+
+                using (MySqlCommand command = new MySqlCommand(insertQuery, conection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Apellidos", apellidos);                    
+                    command.ExecuteNonQuery();
+                }
+                conection.Close();
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            btnDesconectar.Enabled = true;
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                    ArduinoPort.Open();
+                if (int.TryParse(txtLimTem.Text, out int temperatureLimit))
+                {
+                    //Convierte
+                    string limitString = temperatureLimit.ToString();
+                    ArduinoPort.Write(limitString);
+                }
+                else
+                {
+                    MessageBox.Show("Ingresa un valor númerico válido en el TextBox del limite de la Temperatura. ");
+                }
+
+                lblConetion.Text = "Conexión OK";
+                lblConetion.ForeColor = System.Drawing.Color.Lime;
+            }
+
+            catch
+            {
+                MessageBox.Show("Configure el puerto de comunicacion correcto o Desconecte");
+            }
+
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            btnConectar.Enabled = true;
+            btnDesconectar.Enabled = false;
+            if (ArduinoPort.IsOpen)
+                ArduinoPort.Close();
+            lblConetion.Text = "Desconectado";
+            lblConetion.ForeColor = System.Drawing.Color.Red;
+            lblTemperatura.Text = "0.0";
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        //Registo de Datos en MYSQL
+        private void InsertarRegistro2(string encender)
+        {
+            using (MySqlConnection conection = new MySqlConnection(conexionSql))
+            {
+                conection.Open();
+
+                string insertQuery = "INSERT INTO registros (Led)" + "VALUES (@Led)";
+
+                using (MySqlCommand command = new MySqlCommand(insertQuery, conection))
+                {
+                    command.Parameters.AddWithValue("@Led", encender);                    
+                    command.ExecuteNonQuery();
+                }
+                conection.Close();
+            }
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+            string encender = btnEncender.Text;
+            InsertarRegistro2(encender);
+        }
+        private void CerrandoForm1(object sender, EventArgs e)
+        {
+            //Cerrando Puerto 
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Obtener los datos en los TexBox 
+            string nombres = txtNombre.Text;
+            string apellidos = txtApellido.Text;
+            InsertarRegistro(nombres, apellidos);            
+            MessageBox.Show("Datos ingresados Correctamente");
+        }
+        
+    }
+}
